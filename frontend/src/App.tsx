c95788efa10b29e080c03de56a1b15de8b33eff0
Eden <cheungedenhk@gmail.com>
2023-04-25 10:12:11 +0100

    Add pill to selected link and improve mobile paddings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/navbar';
 
-
 function App() {
 	return (
-		<div className="App">
-			<Navbar/>
-		</div>
+		<React.StrictMode>
+			<div className="App">
+				<BrowserRouter>
+					<Navbar />
+					<Routes>
+						<Route path="/about">{/* <About /> */}</Route>
+						<Route path="/contact">{/* <Contact /> */}</Route>
+						<Route path="/">{/* <Home /> */}</Route>
+					</Routes>
+				</BrowserRouter>
+			</div>
+		</React.StrictMode>
 	);
 }
 
--- frontend/src/components/navbar.tsx
+++ frontend/src/components/navbar.tsx
@@ -1,24 +1,25 @@
-import logo from './../logo1.png';
-
 import { Nav, Navbar } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from './../logo1.png';
 
 const NavbarComp = () => {
 	return (
 		<Navbar bg="dark" variant="dark" sticky="top" expand="xl" collapseOnSelect>
-			<Navbar.Brand id="nav-brand" href="/">
+			<Navbar.Brand id="nav-brand" as={Link} to='/'>
 				<img id="logo" src={logo} width="60px" height="60px" alt="test" />{' '}
 				<span>Solar System Simulator</span>
 			</Navbar.Brand>
 
 			<Navbar.Toggle/>
 			<Navbar.Collapse className='justify-content-end'>
-        <Nav variant='pills'>
-					<Nav.Link href="#1">Kepler's Third Law</Nav.Link> {/*Task 1*/}
-					<Nav.Link href="#2">Orbit Plots</Nav.Link> {/*Task 2+3+4*/}
-					<Nav.Link href="#3">Orbital Time v.s. Polar Angle</Nav.Link> {/*Task 5*/}
-					<Nav.Link href="#4">Spriograph</Nav.Link> {/*Task 6*/}
-					<Nav.Link href="#5">Plolemy Plots</Nav.Link> {/*Task 7*/}
-					<Nav.Link href="#6">More</Nav.Link> {/*Report + Repository*/}
+				<Nav variant='pills'>
+					<LinkContainer to="/keplers-third-law"><Nav.Link>Kepler's Third Law</Nav.Link></LinkContainer> {/*Task 1*/}
+					<LinkContainer to="/orbital-time-vs-polar-angle"><Nav.Link>Orbital Time v.s. Polar Angle</Nav.Link></LinkContainer> {/*Task 5*/}
+					<LinkContainer to="/orbit-plots"><Nav.Link>Orbit Plots</Nav.Link></LinkContainer> {/*Task 2+3+4*/}
+					<LinkContainer to="/spriograph"><Nav.Link>Spriograph</Nav.Link></LinkContainer> {/*Task 6*/}
+					<LinkContainer to="/plolemy-plots"><Nav.Link>Plolemy Plots</Nav.Link></LinkContainer> {/*Task 7*/}
+					<LinkContainer to="/more"><Nav.Link>More</Nav.Link></LinkContainer> {/*Report + Repository*/}
 				</Nav>
 			</Navbar.Collapse>
 		</Navbar>
